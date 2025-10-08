f7920b8f28c279b33cba94f7c5bd1b45011a4050
Michael <Michael.Curry@levrx.com>
2025-10-08 08:19:41 -0400

    maybe this will work@!!!

--- functions/migrate.ts
+++ functions/migrate.ts
@@ -1,9 +1,9 @@
-import { execSync } from "child_process";
 import { Sequelize } from 'sequelize'
 import { Umzug, SequelizeStorage } from 'umzug'
+import { Resource } from 'sst'
 
 export const handler = async (event: any) => {
-  const dbUrl = process.env.DATABASE_URL!
+  const dbUrl = `mysql://${Resource.database.username}:${Resource.database.password}@${Resource.database.host}:${Resource.database.port}/${Resource.database.database}` // process.env.DATABASE_URL!
 
   const sequelize = new Sequelize(dbUrl, {
     dialect: 'mysql',
